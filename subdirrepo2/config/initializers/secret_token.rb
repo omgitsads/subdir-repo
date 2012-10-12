5f290b45540dc0b237f0ca81f41e9c308303918d
Adam Holt <me@adamholt.co.uk>
2012-10-12 14:54:31 +0100

    add a second app

---
+++ subdirrepo2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subdirrepo2::Application.config.secret_token = '2d754b898fd3126ebfddc6279157cb6916a56f3bafcf92a5562b003c410d96f8fa41dd0106fa1da7f252310895d0a451aa41a4c5f38079c594d2407a6bf27eb3'
