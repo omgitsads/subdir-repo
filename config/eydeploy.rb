41e430becb59623eba28d6be801495d7326c0198
Adam Holt <me@adamholt.co.uk>
2012-10-12 14:39:41 +0100

    remove classes

--- config/eydeploy.rb
+++ config/eydeploy.rb
@@ -1,15 +1,8 @@
-module EY
-  module Serverside
-    class DeployBase < Task
-      def copy_repository_cache
-        raise 'COPYING RELEASE'
-        shell.status "Copying to #{paths.active_release}"
-        exclusions = Array(config.copy_exclude).map { |e| %|--exclude="#{e}"| }.join(' ')
-        run("mkdir -p #{paths.active_release} #{paths.releases_failed} #{paths.shared_config} && rsync -aq #{exclusions} #{paths.repository_cache}/app #{paths.active_release}")
-
-        shell.status "Ensuring proper ownership."
-        sudo("chown -R #{config.user}:#{config.group} #{paths.active_release} #{paths.releases_failed}")
-      end
-    end
-  end
+def copy_repository_cache
+  raise 'COPYING RELEASE'
+  shell.status "Copying to #{paths.active_release}"
+  exclusions = Array(config.copy_exclude).map { |e| %|--exclude="#{e}"| }.join(' ')
+  run("mkdir -p #{paths.active_release} #{paths.releases_failed} #{paths.shared_config} && rsync -aq #{exclusions} #{paths.repository_cache}/app #{paths.active_release}")
+  shell.status "Ensuring proper ownership."
+  sudo("chown -R #{config.user}:#{config.group} #{paths.active_release} #{paths.releases_failed}")
 end
