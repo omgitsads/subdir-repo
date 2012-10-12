4c7d2e89323e3ba0a3d4de97b4ebc7014d4d75e9
Adam Holt <me@adamholt.co.uk>
2012-10-12 14:28:44 +0100

    raise

--- config/eydeploy.rb
+++ config/eydeploy.rb
@@ -2,6 +2,7 @@ module EY
   module Serverside
     class DeployBase < Task
       def copy_repository_cache
+        raise 'COPYING RELEASE'
         shell.status "Copying to #{paths.active_release}"
         exclusions = Array(config.copy_exclude).map { |e| %|--exclude="#{e}"| }.join(' ')
         run("mkdir -p #{paths.active_release} #{paths.releases_failed} #{paths.shared_config} && rsync -aq #{exclusions} #{paths.repository_cache}/app #{paths.active_release}")
